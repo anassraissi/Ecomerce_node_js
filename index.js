630882fb0d7facd139a1c884c46616b490868d5c
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-03-06 21:30:04 +0100

    Add eslint to enforce code style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express=require('express');
-const app=express();
-app.get('/',function(req,res){
-    return res.send('hello world!');
+const express = require('express')
+const app = express()
+app.get('/', function (req, res) {
+  return res.send('hello world!')
 })
 
-app.listen(3000,function(){
-    console.log('Server running at port 3000');
+app.listen(3000, function () {
+  console.log('Server running at port 3000')
 })
